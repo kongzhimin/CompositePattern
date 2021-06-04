2bc4a1dc99ff70718b265db27ba6098838697600
1434988286@qq.com <kisses20000222>
2021-06-04 11:09:11 +0800

    Initial commit

---
+++ src/Plate.java
@@ -0,0 +1,20 @@
+import javax.jws.Oneway;
+import java.util.*;
+
+public class Plate extends MyElement{
+    private  ArrayList list = new ArrayList();
+
+    public void add(MyElement element){
+        list.add(element);
+    }
+
+    public void remove(MyElement element){
+        list.remove(element);
+    }
+
+    public void eat(){
+        for(Object object:list){
+            ((MyElement)object).eat();
+        }
+    }
+}
